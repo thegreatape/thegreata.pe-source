626fffd7f9d77333f682f9f571571fd2dfdfa12f
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2020-02-15 20:03:55 -0500

    mostly-final reading page

--- reading.md
+++ reading.md
@@ -1,12 +1,5 @@
 ---
-layout: post
+layout: reading
 permalink: /reading/index.html
 title: Reading
 ---
-
-## Currently Reading
-{% include currently_reading.html %}
-
-## By Year
-
-{% include reading_by_year.html %}
--- sync_reading.rb
+++ sync_reading.rb
@@ -24,70 +24,64 @@ class Reading < Airrecord::Table
 end
 
 class SyncReading
-  def self.save_currently_reading_data(filename)
-    print "saving currently reading books... "
-    readings = Reading.all(filter: "AND(AND({Started On} != BLANK(), {Finished On} = BLANK()), {Stopped Reading On} = BLANK())")
-    File.open(filename, 'wb') do |file|
-      file << JSON.dump({ "readings" => readings.map {|r| r.fields.merge("Book" => r.book.fields)} })
-    end
-    puts "done"
-  end
-
-  def self.save_all_finished(filename)
-    print "saving finished books... "
-    readings = Reading.all(filter: "{Finished On} != BLANK()")
-    File.open(filename, 'wb') do |file|
-      file << JSON.dump({ "readings" => readings.map {|r| r.fields.merge("Book" => r.book.fields)} })
-    end
-    puts "done"
-  end
-
-  def self.render(json_filename:, template:, html_filename:)
-    json = JSON.parse(File.read(json_filename))
-    if block_given?
-      json = yield json
-    end
-    File.open(html_filename, 'wb') do |file|
-      file <<  Haml::Engine.new(File.read(template)).render(Object.new, json)
-    end
-  end
-end
-
-#SyncReading.save_currently_reading_data('reading/currently_reading.json')
-#SyncReading.save_all_finished('reading/finished.json')
-
-SyncReading.render(
-  json_filename: 'reading/finished.json', 
-  template:'reading/templates/reading_by_year.haml',
-  html_filename:'_includes/reading_by_year.html'
-) do |json|
-  by_year = json["readings"].group_by {|r| r["Read In Year"] }.map do |year, readings|
-    next if year == "Not Yet Finished"
-
-    stats = {
-      "Author Background" => Hash.new { 0 },
-      "Genres" => Hash.new { 0 }
-    }
-    readings.each do |reading|
-      stats.keys.each do |key|
-        next unless reading["Book"][key]
-        reading["Book"][key].each do |value|
-          stats[key][value] += 1
-        end
+  def currently_reading
+    @_currently_reading ||= begin
+      print "saving currently reading books... "
+      readings = Reading.all(filter: "AND(AND({Started On} != BLANK(), {Finished On} = BLANK()), {Stopped Reading On} = BLANK())")
+      readings.map {|r| r.fields.merge("Book" => r.book.fields)}.tap do
+        puts "done"
       end
     end
+  end
 
-    data = {
-      "readings" => readings,
-      "stats" => stats,
+  def finished
+    @_finished ||= begin
+      print "saving finished books... "
+      readings = Reading.all(filter: "{Finished On} != BLANK()")
+      readings.map {|r| r.fields.merge("Book" => r.book.fields)}.tap do
+        puts "done"
+      end
+    end
+  end
+
+  def by_year
+    finished.group_by {|r| r["Read In Year"] }.map do |year, readings|
+      next if year == "Not Yet Finished"
+
+      stats = {
+        "Author Background" => Hash.new { 0 },
+        "Genres" => Hash.new { 0 }
+      }
+      readings.each do |reading|
+        stats.keys.each do |key|
+          next unless reading["Book"][key]
+          reading["Book"][key].each do |value|
+            stats[key][value] += 1
+          end
+        end
+      end
+
+      data = {
+        "readings" => readings,
+        "stats" => stats,
+      }
+      [year, data]
+    end.compact.sort.reverse
+  end
+
+  def save(book_data, filename)
+    File.open(filename, 'wb') do |file|
+      file << JSON.dump(book_data)
+    end
+  end
+
+  def save_reading_history(filename)
+    book_data = {
+      "currently_reading" => currently_reading,
+      "by_year" => by_year
     }
-    [year, data]
-  end.compact.sort.reverse
-  { "years" => by_year }
+    save(book_data, filename)
+  end
 end
 
-SyncReading.render(
-  json_filename: 'reading/currently_reading.json', 
-  template:'reading/templates/currently_reading.haml',
-  html_filename:'_includes/currently_reading.html'
-)
+SyncReading.new.save_reading_history('reading/books.json')
