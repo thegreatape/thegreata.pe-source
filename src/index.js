1af67aa28c1ab628547e5da535eec53dfeb2a6f7
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2020-03-23 21:00:27 -0400

    Add reading goals and short explanation.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 require('./scss/styles.scss');
 require('./js/hamburger.js');
 require('./js/time-ago.js');
+require('./js/reading-progress.js');
---
+++ src/js/reading-progress.js
@@ -0,0 +1,25 @@
+import $ from "cash-dom";
+
+$(function() {
+  const { goal, read } = $('#progress-to-date').data();
+  const today = new Date();
+  const yearStart = new Date(today.getFullYear(), 0, 1);
+  const daysSoFar = (today - yearStart) / 1000 / 60 / 60 / 24;
+  const goalBooksPerDay = goal / 365.0;
+  const goalBooksByNow = Math.round(goalBooksPerDay * daysSoFar);
+  const difference = Math.abs(read - goalBooksByNow);
+
+  var message;
+  var books = 'books';
+  if(difference == 1) {
+    books = 'book';
+  }
+  if(goalBooksPerDay == read) {
+    message = 'On track!';
+  } else if (goalBooksPerDay > read) {
+    message = `${difference} ${books} ahead!`;
+  } else {
+    message = `${difference} ${books} behind.`;
+  }
+  $('#progress-to-date').text(message);
+});
